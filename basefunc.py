671b11d1e9262a59c4cbbb8cccad2450136678d2
Veera Elluru <Veera.Raghavendra@genesys.com>
2019-08-28 12:48:21 +0530

    writeJsonFile

--- basefunc.py
+++ basefunc.py
@@ -6,3 +6,9 @@ from os.path import basename, splitext
 from os.path import isfile, isdir, join
 from os import listdir
 from os import mkdir
+
+def writeJsonFile(dictList, keyname, outJsonFile):
+    intentsDict = {keyname:dictList}
+    jsonData = json.dumps(intentsDict, indent=2, sort_keys=True, separators=(',', ':'))
+    with open(outJsonFile, "w") as jFile:
+        jFile.write(jsonData)
