bd797a81753e816bc106d8690ee1e9e481638d15
Veera Elluru <Veera.Raghavendra@genesys.com>
2019-08-28 14:47:39 +0530

    load_dictionary

--- basefunc.py
+++ basefunc.py
@@ -6,9 +6,14 @@ from os.path import basename, splitext
 from os.path import isfile, isdir, join
 from os import listdir
 from os import mkdir
+import json
 
 def writeJsonFile(dictList, keyname, outJsonFile):
     intentsDict = {keyname:dictList}
     jsonData = json.dumps(intentsDict, indent=2, sort_keys=True, separators=(',', ':'))
     with open(outJsonFile, "w") as jFile:
         jFile.write(jsonData)
+
+def load_dictionary(infile, split_char=' '):
+    phoneDict = dict()
+    lines = readFile(infile)
